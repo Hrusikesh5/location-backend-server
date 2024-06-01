78f937112de7fe77f6a1393a9157ae2ad072e326
drivadosayan <techsupport1@drivado.com>
2024-06-01 18:14:31 +0530

    added

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,12 @@ config :live_track, LiveTrack.RedisClient,
 
 config :live_track, LiveTrackWeb.Endpoint,
   url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME"), port: 443],
-  check_origin: ["https://location-backend-server.onrender.com", "//localhost", "//127.0.0.1"],
+  check_origin: [
+    "https://location-backend-server.onrender.com",
+    "https://665b14fc979a3ea708b1f14d--tourmaline-moxie-e22666.netlify.app/",
+    "//localhost",
+    "//127.0.0.1"
+  ],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")]
